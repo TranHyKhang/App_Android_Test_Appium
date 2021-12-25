e1344ffa72a765cd58f1cb9f5abc3cb12bed7219
TranHyKhang <tranhykhang8108@gmail.com>
2021-12-25 16:10:50 +0700

    Bottom tab page object

--- src/page_objects/BottomTab/BottomTab.js
+++ src/page_objects/BottomTab/BottomTab.js
@@ -8,7 +8,25 @@ const NEWS_TAB = '//android.widget.FrameLayout[@content-desc="News"]';
 
 const PROFILE_TAB = '//android.widget.FrameLayout[@content-desc="Profile"]';
 
+const HOME_TAB = '//android.widget.FrameLayout[@content-desc="Home"]';
+const HOME_LABEL = '//android.widget.FrameLayout[@content-desc="Home"]/android.view.ViewGroup/android.widget.TextView';
+
 class BottomTab {
+
+    get getHomeTab() {
+        $(HOME_TAB).waitForDisplayed({timeout: 20000})
+        return $(HOME_LABEL);
+    }
+
+    get getHomeTabLabel() {
+        $(HOME_LABEL).waitForDisplayed({timeout: 20000});
+        return $(HOME_LABEL);
+    }
+
+    clickOnHomeTab() {
+        this.getHomeTab.click();
+    }
+
     get getVoucherTab() {
         $(VOUCHER_TAB).waitForDisplayed({timeout: 20000})
         return $(VOUCHER_TAB);
