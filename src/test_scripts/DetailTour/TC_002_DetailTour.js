34396a52ae5b21a2436032ca51ac4289214a3e67
TranHyKhang <tranhykhang8108@gmail.com>
2021-12-25 16:53:48 +0700

    Detail tour test case

---
+++ src/test_scripts/DetailTour/TC_002_DetailTour.js
@@ -0,0 +1,85 @@
+import Login from '../../page_objects/account/Login'
+import OTP from '../../page_objects/account/OTP'
+import DetailTour from '../../page_objects/DetailTour/DetailTour';
+import HomeScreen from '../../page_objects/HomeScreen/HomeScreen';
+
+describe('Test Detail Tour function', () => {
+    it('Login to Home', () => {
+
+        Login.setPhoneNumberTextField('0933308108').clickOnButtonLogin();
+
+        OTP.OTPTextField().setOTPTextField('000000').clickOnButtonConfirmOTP();
+
+        browser.pause(5000);
+        
+    })
+
+    it('Swipe vertically', () => {
+
+        /********SWIPE SCREEN VERTICALLY *******/
+        const {height} = driver.getWindowSize();
+        const anchorPercentage = 50;
+        const startPointPercentage = 90;
+        const endPointPercentage = 10;
+
+        const anchor = height * anchorPercentage / 100;
+        const startPoint = height * startPointPercentage / 100;
+        const endPoint = height * endPointPercentage / 100;
+
+        driver.touchPerform([
+            {
+                action: 'press',
+                options: {
+                    x: anchor,
+                    y: startPoint
+                }
+            },
+            {
+                action: 'wait',
+                options: {
+                    ms: 1000
+                }
+            },
+            {
+                action: 'moveTo',
+                options: {
+                    x: anchor,
+                    y: endPoint
+                }
+            },
+            {
+                action: 'release',
+                options: {
+
+                }
+            }
+        ])
+
+        /**********************************/
+
+    })
+
+    it('Click tour item', () => {
+        HomeScreen.clickOnImageListTourItem();
+
+        let currentActivity = browser.getCurrentActivity();
+        console.log(a);
+
+        let expectActivity = currentActivity == '.InfoTour' ? true : false
+
+        expect(expectActivity).toBe(true);
+        
+        browser.pause(5000);
+
+    })
+
+    it('Click button desc and info', () => {
+        DetailTour.clickOnButtonDesc();
+
+        browser.pause(3000)
+
+        DetailTour.clickOnButtonInfo();
+
+        browser.pause(5000)
+    })
+})
