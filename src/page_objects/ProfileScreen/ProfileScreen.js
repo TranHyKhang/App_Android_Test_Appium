c2016d69da031f8ba70c9a4883b73a8bd4774b23
TranHyKhang <tranhykhang8108@gmail.com>
2021-12-27 16:51:10 +0700

    Update ProfileScreen page object

---
+++ src/page_objects/ProfileScreen/ProfileScreen.js
@@ -0,0 +1,33 @@
+const BUTTON_PROFILE = '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[1]/android.view.ViewGroup/android.widget.LinearLayout/android.widget.LinearLayout/android.widget.TextView[2]';
+
+const BUTTON_LOVED = '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[1]/android.view.ViewGroup/android.widget.TableLayout/android.widget.TableRow[1]/android.widget.FrameLayout[1]/android.widget.LinearLayout/android.widget.ImageView';
+const BUTTON_HISTORY = '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[1]/android.view.ViewGroup/android.widget.TableLayout/android.widget.TableRow[1]/android.widget.FrameLayout[2]/android.widget.LinearLayout/android.widget.ImageView';
+const BUTTON_LANGUAGE = '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[1]/android.view.ViewGroup/android.widget.TableLayout/android.widget.TableRow[2]/android.widget.FrameLayout[1]/android.widget.LinearLayout/android.widget.ImageView';
+const BUTTON_RULES = '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[1]/android.view.ViewGroup/android.widget.TableLayout/android.widget.TableRow[2]/android.widget.FrameLayout[2]/android.widget.LinearLayout/android.widget.ImageView';
+const BUTTON_CONTACT = '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[1]/android.view.ViewGroup/android.widget.TableLayout/android.widget.TableRow[3]/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.ImageView';
+
+class ProfileScreen {
+    waitTillDisplayed() {
+        $(BUTTON_PROFILE).waitForDisplayed({timeout: 5000})
+    }
+
+    clickOnButtonProfile() {
+        $(BUTTON_PROFILE).click();
+    }
+
+    clickOnButtonLoved() {
+        $(BUTTON_LOVED).click();
+    }
+
+    clickOnButtonHistory() {
+        $(BUTTON_HISTORY).click();
+    }
+
+    clickOnButtonRules() {
+        $(BUTTON_RULES).click();
+    }
+
+    clickOnButtonContact() {
+        $(BUTTON_CONTACT).click();
+    }
+}
