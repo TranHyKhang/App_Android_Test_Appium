847d4107efc7d422d1082abfaf56f64be5b3cb29
TranHyKhang <tranhykhang8108@gmail.com>
2021-12-27 16:50:41 +0700

    Update DetailTour page object

--- src/page_objects/DetailTour/DetailTour.js
+++ src/page_objects/DetailTour/DetailTour.js
@@ -11,6 +11,8 @@ const CHILDREN_NUMBER = '/hierarchy/android.widget.FrameLayout/android.widget.Li
 const CHILDREN_INCREASE_BUTTON = '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.widget.ScrollView/android.widget.LinearLayout/android.widget.LinearLayout/android.widget.FrameLayout[2]/android.view.ViewGroup/android.widget.FrameLayout[2]/android.widget.LinearLayout/android.widget.LinearLayout[2]/android.widget.LinearLayout/android.widget.Button[2]';
 const CHILDREN_DECREASE_BUTTON = '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.widget.ScrollView/android.widget.LinearLayout/android.widget.LinearLayout/android.widget.FrameLayout[2]/android.view.ViewGroup/android.widget.FrameLayout[2]/android.widget.LinearLayout/android.widget.LinearLayout[2]/android.widget.LinearLayout/android.widget.Button[1]';
 
+const TOUR_PRICE = '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.widget.ScrollView/android.widget.LinearLayout/android.widget.LinearLayout/android.widget.FrameLayout[2]/android.view.ViewGroup/android.widget.FrameLayout[1]/android.widget.LinearLayout/android.widget.LinearLayout[5]/android.widget.TextView[2]';
+
 const BOOK_TOUR_BUTTON = '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.widget.ScrollView/android.widget.LinearLayout/android.widget.LinearLayout/android.widget.FrameLayout[2]/android.view.ViewGroup/android.widget.FrameLayout[3]/android.widget.Button';
 
 
@@ -136,6 +138,11 @@ class DetailTour {
     //BOOK BUTTON
     clickOnButtonBookTour() {
         $(BOOK_TOUR_BUTTON).click();
+    }
+
+    //TOUR PRICE
+    get getTourPrice() {
+        return $(TOUR_PRICE).getText().replace(/,/g,'');
     }
 }
 
