b5d57115f4532138d7a255ac57a4e5405433cd08
TranHyKhang <tranhykhang8108@gmail.com>
2021-12-31 18:09:19 +0700

    RuleScreen test case

---
+++ src/test_scripts/RulesScreen/TC_001_RulesScreen.js
@@ -0,0 +1,45 @@
+import Login from '../../page_objects/account/Login';
+import OTP from '../../page_objects/account/OTP';
+import BottomTab from '../../page_objects/BottomTab/BottomTab';
+import LanguageScreen from '../../page_objects/ProfileScreen/LanguageScreen';
+import LoveScreen from '../../page_objects/ProfileScreen/LoveScreen';
+import ProfileScreen from '../../page_objects/ProfileScreen/ProfileScreen';
+
+
+describe('Bottom tab Function', () => {
+    it('Login to Home', () => {
+
+        Login.setPhoneNumberTextField('0933308108').clickOnButtonLogin();
+
+        OTP.OTPTextField().setOTPTextField('000000').clickOnButtonConfirmOTP();
+
+        browser.pause(5000);
+    })
+
+    it('Tab profile tab', () => {
+        BottomTab.clickOnProfileTab();
+
+        browser.pause(1000)
+    })
+
+    it('Click on Rules', () => {
+        ProfileScreen.waitTillDisplayed().clickOnButtonRules();
+
+        browser.pause(2000);
+
+        let currentActivity = browser.getCurrentActivity();
+
+        console.log('***************');
+        console.log(currentActivity)
+        console.log('***************');
+
+        let expectActivity = currentActivity === '.Rules' ? true: false;
+
+        expect(expectActivity).toBe(true);
+
+
+        browser.pause(5000)
+
+    })
+
+})
