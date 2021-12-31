ba73f968d9f39e0f05b2b2e5a7b0bed661ca1ae8
TranHyKhang <tranhykhang8108@gmail.com>
2021-12-31 11:51:05 +0700

    LoveScreen page object

---
+++ src/page_objects/ProfileScreen/LoveScreen.js
@@ -0,0 +1,21 @@
+const IMAGE_BUTTON = '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/androidx.recyclerview.widget.RecyclerView/android.widget.FrameLayout[1]/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.ImageView';
+
+const HEART_ICON = '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/androidx.recyclerview.widget.RecyclerView/android.widget.FrameLayout[1]/android.widget.LinearLayout/android.widget.LinearLayout/android.widget.LinearLayout/android.widget.ImageView';
+
+const ICON_BACK = '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.ImageView';
+
+class LoveScreen {
+    waitTillDisplayed() {
+        $(ICON_BACK).waitForDisplayed({timeout: 5000});
+    }
+
+    clickOnImageButton() {
+        $(IMAGE_BUTTON).click();
+    }
+
+    clickOnHeartIcon() {
+        $(HEART_ICON).click();
+    }
+}
+
+export default new LoveScreen();
